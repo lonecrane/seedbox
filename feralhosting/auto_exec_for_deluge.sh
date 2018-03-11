d2fc1170b4d3369d6b0278575e328ba7f451782f
lonecrane <36885830+lonecrane@users.noreply.github.com>
2018-03-11 09:44:35 +0800

    Update auto_exec_for_deluge.sh

--- feralhosting/auto_exec_for_deluge.sh
+++ feralhosting/auto_exec_for_deluge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
-exec >/tmp/script$$.output 2>&1
+exec >/tmp/script-$0.output 2>&1
 
 torrentid=$1
 torrentname=$2
 torrentpath=$3
-echo "Torrent Details: " "$torrentname" "$torrentpath" "$torrentid"  >> /tmp/de_execute_script_111.log
+echo "Torrent Details: " "$torrentname" "$torrentpath" "$torrentid"  >> ~/tmp/script-$0.output
 
 #此处替换为实际的用户名
 xxx=???
@@ -13,4 +13,4 @@ SOURCE_TORR=/media/md3/$xxx/.config/deluge/state
 DEST_TORR=/media/md3/$xxx/torrent/completed
 
 echo `pwd`  >> /tmp/de_execute_script_111.log
-cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR/$torrentid.torrent  >> /tmp/de_execute_script_111.log
+cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR/$torrentid.torrent  >> ~/tmp/script-$0.output

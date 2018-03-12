b073329f9c1f9411a8c136f8a406c9c38babe26e
lonecrane <36885830+lonecrane@users.noreply.github.com>
2018-03-12 16:09:09 +0800

    Update auto_exec_for_deluge.sh

--- feralhosting/auto_exec_for_deluge.sh
+++ feralhosting/auto_exec_for_deluge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
-exec >/tmp/script-$0.output 2>&1
+exec >> ~/tmp/script-auto-exec-deluge-$$.output 2>&1
 
 torrentid=$1
 torrentname=$2
 torrentpath=$3
-echo "Torrent Details: " "$torrentname" "$torrentpath" "$torrentid"  >> ~/tmp/script-$0.output
+echo "Torrent Details: " "$torrentname" "$torrentpath" "$torrentid"  >> ~/tmp/script-auto-exec-deluge-$$.log
 
 #此处替换为实际的用户名
 xxx=???
@@ -13,4 +13,6 @@ SOURCE_TORR=/media/md3/$xxx/.config/deluge/state
 DEST_TORR=/media/md3/$xxx/torrent/completed
 
 echo `pwd`  >> /tmp/de_execute_script_111.log
-cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR/$torrentid.torrent  >> ~/tmp/script-$0.output
+cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR-for-rt/$torrentid.torrent  >> ~/tmp/script-auto-exec-deluge-$$.log
+#cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR-bak/$torrentid.torrent
+#cp $SOURCE_TORR/$torrentid.torrent $DEST_TORR-for-qb/$torrentid.torrent
